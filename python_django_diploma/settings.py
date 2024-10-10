5718632729e50fab1efad8699d5fce0b19b3043f
Riddhub <unkownrid@gmail.com>
2024-10-10 13:59:14 +0300

    - install drf-spectacular

--- python_django_diploma/settings.py
+++ python_django_diploma/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
 
     'frontend',
     'rest_framework',
+    'drf_spectacular',
     'product_app.apps.ProductAppConfig',
-
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
