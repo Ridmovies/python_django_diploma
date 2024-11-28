74e0fed98646ed5313c3e9fd4f59df8d9aa5506f
Riddhub <unkownrid@gmail.com>
2024-11-28 10:31:17 +0300

    clean payment

--- payment_app/urls.py
+++ payment_app/urls.py
@@ -1,16 +1,7 @@
 from django.urls import path
 
-from payment_app.views import (
-    # PaymentConfirmView,
-    PaymentInfoView,
-    PaymentListView,
-    PaymentNotification,
-)
+from payment_app.views import PaymentNotification
 
 urlpatterns = [
-    path("payments", PaymentListView.as_view()),
     path("payments/<str:payment_id>/success", PaymentNotification.as_view()),
-    path("payments/<str:payment_id>", PaymentInfoView.as_view()),
-    # path("payments/<str:payment_id>/capture", PaymentConfirmView.as_view()),
-    # path("payment/<int:id>", PaymentView.as_view()),
 ]
