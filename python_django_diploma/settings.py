fdc717b104e4fe9d97da42dc1b5c277b21eb493f
riddler_rid <unkownrid@gmail.com>
2024-12-05 12:03:10 +0300

    - some fix

--- python_django_diploma/settings.py
+++ python_django_diploma/settings.py
@@ -159,6 +159,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Настройка STATIC_URL
+STATIC_URL = '/static/'
+
 if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / "static",
